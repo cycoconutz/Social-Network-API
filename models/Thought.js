888b1d10f6d92c2244dcd6b3124f4c0fceb5ee3e
danny <johndyates@gmail.com>
2022-05-24 15:27:37 -0500

    initial commit

---
+++ models/Thought.js
@@ -0,0 +1,34 @@
+const { Schema, Types } = require('mongoose');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: formatDate,
+    },
+    score: {
+      type: Number,
+      required: true,
+      default: () => Math.floor(Math.random() * (100 - 70 + 1) + 70),
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = assignmentSchema;
