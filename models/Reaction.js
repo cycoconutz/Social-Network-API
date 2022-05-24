2538993113256c89da80285999e68b8f56276f03
danny <johndyates@gmail.com>
2022-05-24 15:38:12 -0500

    models complete

---
+++ models/Reaction.js
@@ -0,0 +1,34 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: formatDate,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+function formatDate (date) {
+  const timeElapsed = date;
+  const today = new Date(timeElapsed);
+  return today.toUTCString();
+}
+
+module.exports = reactionSchema;
--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const { Schema, Types } = require('mongoose');
+const Reaction = require('./Reaction');
 
 const thoughtSchema = new Schema(
   {
@@ -13,22 +14,32 @@ const thoughtSchema = new Schema(
       default: Date.now,
       get: formatDate,
     },
-    score: {
-      type: Number,
+    username: {
+      type: String,
       required: true,
-      default: () => Math.floor(Math.random() * (100 - 70 + 1) + 70),
     },
-    createdAt: {
-      type: Date,
-      default: Date.now,
+    reactions: [Reaction],   
     },
-  },
   {
     toJSON: {
+      virtuals: true,
       getters: true,
     },
     id: false,
   }
 );
 
-module.exports = assignmentSchema;
+function formatDate (date) {
+  const timeElapsed = date;
+  const today = new Date(timeElapsed);
+  return today.toUTCString();
+}
+
+thoughtSchema.virtual('reactionCount')
+.get(function () {
+    return `Number of reaction: ${this.reactions.length}`;
+})
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
